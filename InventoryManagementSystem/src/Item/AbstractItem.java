78d755eba1cdb64e17b15aaf0c7ec689abf1ab49
NGNikolay <georgiev.nikolaj@gmail.com>
2024-06-26 15:18:57 +0300

    finished the abstract item class

--- InventoryManagementSystem/src/Item/AbstractItem.java
+++ InventoryManagementSystem/src/Item/AbstractItem.java
@@ -5,12 +5,11 @@ import java.time.LocalDate;
 public abstract class AbstractItem implements Item, Categorizable, Breakable, Perishable, Sellable {
     protected String name;
     protected String description;
-    protected int price;
-    protected int quantity;
+    protected double price;
     protected String category;
     protected boolean perishable;
-    protected boolean sellable;
     protected boolean broken;
+    protected LocalDate initialDate;
 
     @Override
     public boolean isBroken() {
@@ -19,22 +18,17 @@ public abstract class AbstractItem implements Item, Categorizable, Breakable, Pe
 
     @Override
     public void setBroken(boolean b) {
-        if(b){
+        if (b) {
             this.broken = true;
         }
     }
 
     @Override
     public String getItemStatus() {
-        if(broken){
+        if (broken) {
             return "Broken";
         }
         return "Not Broken";
-    }
-
-    @Override
-    public void setCategory(String category) {
-        this.category = category;
     }
 
     @Override
@@ -43,67 +37,81 @@ public abstract class AbstractItem implements Item, Categorizable, Breakable, Pe
     }
 
     @Override
-    public boolean isCategorizable() {
-        return false;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     @Override
     public String getName() {
-        return "";
+        return this.name;
     }
 
     @Override
     public void setName(String name) {
-
+        if (name != null && !name.isEmpty()) {
+            this.name = name;
+        }
     }
 
     @Override
     public String getDescription() {
-        return "";
+        return this.description;
     }
 
     @Override
     public void setDescription(String description) {
-
+        if (description != null && !description.isEmpty()) {
+            this.description = description;
+        }
     }
 
     @Override
     public double calculateValue(int quantity) {
-        return 0;
+        return this.price * quantity;
     }
 
     @Override
     public boolean isPerishable() {
-        return false;
+        return perishable;
     }
 
     @Override
     public void setPerishable(boolean perishable) {
-
+        this.perishable = perishable;
     }
 
     @Override
-    public LocalDate setInitialDate(LocalDate initialDate) {
-        return null;
+    public void setInitialDate(LocalDate initialDate) {
+        this.initialDate = initialDate;
     }
 
     @Override
     public LocalDate calculateExpiryDate(int days) {
-        return null;
+        if (initialDate == null) {
+            throw new IllegalStateException("Initial date not set");
+        }
+        return initialDate.plusDays(days);
     }
 
     @Override
     public void displayPerishable() {
-
+        if (perishable) {
+            System.out.println("Perishable");
+        } else {
+            System.out.println("Not Perishable");
+        }
     }
 
     @Override
     public double getPrice() {
+        if (price != 0) {
+            return price;
+        }
         return 0;
     }
 
     @Override
     public void setPrice(double price) {
-
+        this.price = price;
     }
 }
