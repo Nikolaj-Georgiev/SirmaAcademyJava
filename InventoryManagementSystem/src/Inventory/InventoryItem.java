fd5af57bb3ee44b5217b23b4d2c7cd1f5f7bc4e2
NGNikolay <georgiev.nikolaj@gmail.com>
2024-06-27 16:57:46 +0300

    edited some of the methods to be abstract and to implement them in separate classes

--- InventoryManagementSystem/src/Inventory/InventoryItem.java
+++ InventoryManagementSystem/src/Inventory/InventoryItem.java
@@ -2,15 +2,24 @@ package Inventory;
 
 import Item.AbstractItem;
 
-public class InventoryItem extends AbstractItem {
+import java.time.LocalDate;
+
+public abstract class InventoryItem extends AbstractItem {
     protected String id;
     protected int quantity;
 
-    public InventoryItem(String id, int quantity) {
+
+    public InventoryItem(String id, int quantity, String name, String description, double price, String category, boolean perishable, boolean broken) {
         this.id = id;
         this.quantity = quantity;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.perishable = perishable;
+        this.initialDate = LocalDate.now();
     }
-
+    
     public InventoryItem() {
     }
 
@@ -20,6 +29,11 @@ public class InventoryItem extends AbstractItem {
 
     public InventoryItem(int quantity) {
         this.quantity = quantity;
+    }
+
+    public InventoryItem(String itemName, int quantity) {
+        this.quantity = quantity;
+        this.name = itemName;
     }
 
     public int getQuantity() {
@@ -44,5 +58,16 @@ public class InventoryItem extends AbstractItem {
             return;
         }
         this.id = id;
+    }
+
+    @Override
+    public double calculateValue(int quantity) {
+        return 0;
+    }
+
+
+    @Override
+    public void setInitialDate(LocalDate initialDate) {
+
     }
 }
