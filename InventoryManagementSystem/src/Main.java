44976d6a9dee53c40844d84d8b292c34d03ab33c
NGNikolay <georgiev.nikolaj@gmail.com>
2024-06-27 18:20:05 +0300

    added functionality for collecting the items in a serialized file

--- InventoryManagementSystem/src/Main.java
+++ InventoryManagementSystem/src/Main.java
@@ -1,5 +1,30 @@
+import FileInputOutpus.FileIO;
+import Inventory.ElectronicsItem;
+import Inventory.GroceryItem;
+import Inventory.InventoryItem;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<InventoryItem> inventory = new ArrayList<>();
+        inventory.add(new GroceryItem("1", 10, "Milk", "Fresh milk", 2.99, "Grocery", true, 7));
+        inventory.add(new GroceryItem("2", 20, "Bread", "Freshly baked bread", 1.99, "Grocery", false, 3));
+        inventory.add(new ElectronicsItem("3", 5, "Laptop", "Gaming laptop", 999.99, "Electronics"));
+
+        try {
+            FileIO.saveInventory(inventory);
+            List<InventoryItem> loadedInventory = FileIO.loadInventory();
+
+            for (InventoryItem item : loadedInventory) {
+                System.out.println(item.getId());
+            }
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
