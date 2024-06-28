7dd93574d9029e8bad4bbc363402ddf851e5d44d
NGNikolay <georgiev.nikolaj@gmail.com>
2024-06-28 11:54:01 +0300

    added suoer constructor for better readablility

--- InventoryManagementSystem/src/Inventory/ElectronicsItem.java
+++ InventoryManagementSystem/src/Inventory/ElectronicsItem.java
@@ -4,26 +4,9 @@ import java.time.LocalDate;
 
 public class ElectronicsItem extends InventoryItem {
 
-    public ElectronicsItem() {
-    }
-
-    public ElectronicsItem(String itemName) {
-        setName(itemName);
-    }
-
-    public ElectronicsItem(String itemName, int quantity) {
-        setName(itemName);
-        setQuantity(quantity);
-    }
-
-    public ElectronicsItem(String id, int quantity, String name, String description, double price, String category) {
-        setId(id);
-        setQuantity(quantity);
-        setName(name);
-        setDescription(description);
-        setPrice(price);
-        setCategory(category);
-        setInitialDate(LocalDate.now());
+    public ElectronicsItem(String name, String description, double price, String category, int quantity) {
+        super(name, description, price, category, quantity);
+        this.setInitialDate(LocalDate.now());
     }
 
     @Override
@@ -31,4 +14,5 @@ public class ElectronicsItem extends InventoryItem {
         return this.getPrice() * this.getQuantity();
 
     }
+
 }
