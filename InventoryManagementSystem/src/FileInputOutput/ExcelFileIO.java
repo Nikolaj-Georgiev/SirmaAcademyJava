fd457c163952d720e62b1dbe7900a5e2e34cc75c
NGNikolay <georgiev.nikolaj@gmail.com>
2024-06-28 17:25:11 +0300

    added reading from excel file logic

--- InventoryManagementSystem/src/FileInputOutput/ExcelFileIO.java
+++ InventoryManagementSystem/src/FileInputOutput/ExcelFileIO.java
@@ -1,5 +1,7 @@
 package FileInputOutput;
 
+import Inventory.ElectronicsItem;
+import Inventory.FragileItem;
 import Inventory.GroceryItem;
 import Inventory.InventoryItem;
 import org.apache.poi.ss.usermodel.Cell;
@@ -8,8 +10,11 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static FileInputOutput.ExcelConstants.*;
@@ -57,12 +62,61 @@ public class ExcelFileIO {
         cell = row.createCell(COL_QUANTITY);
         cell.setCellValue(item.getQuantity());
 
+        cell = row.createCell(COL_INITIAL_DATE);
+        cell.setCellValue(item.getInitialDate());
+
         if (item instanceof GroceryItem) {
             GroceryItem gItem = (GroceryItem) item;
             cell = row.createCell(COL_EXP_DAYS);
             cell.setCellValue(gItem.getExpDays());
         }
 //    ако има други специфични колони могат да се направят проверки и за тях
+
+    }
+
+    public static List<InventoryItem> readInventory() throws IOException {
+        List<InventoryItem> inventory = new ArrayList<>();
+
+        try (FileInputStream fileIn = new FileInputStream(FILE_NAME)) {
+            Workbook workbook = new XSSFWorkbook(fileIn);
+            Sheet sheet = workbook.getSheet("Inventory");
+
+            for (Row row : sheet) {
+                InventoryItem item = readItem(row);
+                inventory.add(item);
+            }
+            workbook.close();
+        }
+        return inventory;
+    }
+
+    private static InventoryItem readItem(Row row) {
+        String className = row.getCell(COL_CLASS_NAME).getStringCellValue();
+        String id = row.getCell(COL_ID).getStringCellValue();
+        String name = row.getCell(COL_NAME).getStringCellValue();
+        String description = row.getCell(COL_DESCRIPTION).getStringCellValue();
+        double price = row.getCell(COL_PRICE).getNumericCellValue();
+        String category = row.getCell(COL_CATEGORY).getStringCellValue();
+        int quantity = Integer.parseInt(row.getCell(COL_QUANTITY).getStringCellValue());
+        LocalDate initialDate = LocalDate.parse(row.getCell(COL_INITIAL_DATE).getStringCellValue());
+
+        switch (className) {
+            case "GroceryItem":
+                int expDays = (int) row.getCell(COL_EXP_DAYS).getNumericCellValue();
+                GroceryItem groceryItem = new GroceryItem(name, description, price, category, quantity, expDays);
+                groceryItem.setInitialDate(initialDate);
+                return groceryItem;
+            case "ElectronicItem":
+                ElectronicsItem electronicsItem = new ElectronicsItem(name, description, price, category, quantity);
+                electronicsItem.setInitialDate(initialDate);
+                return electronicsItem;
+            case "FragileItem":
+                FragileItem fragileItem = new FragileItem(name, description, price, category, quantity);
+                fragileItem.setInitialDate(initialDate);
+                return fragileItem;
+            default:
+                throw new IllegalArgumentException(STR."Unknown item type: \{className}");
+        }
 
     }
 }
