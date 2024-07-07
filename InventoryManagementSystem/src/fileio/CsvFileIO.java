a251b0ca9c96393393a97a186fa9fa157c2db75f
NGNikolay <georgiev.nikolaj@gmail.com>
2024-07-07 09:29:51 +0300

    implemented saving logic

--- InventoryManagementSystem/src/fileio/CsvFileIO.java
+++ InventoryManagementSystem/src/fileio/CsvFileIO.java
@@ -12,12 +12,13 @@ public class CsvFileIO {
 
     private static void saveInventory(List<InventoryItem> inventory) {
 
-        try(FileWriter fw = new FileWriter(FILE_NAME)){
+        try (FileWriter fw = new FileWriter(FILE_NAME)) {
+            fw.append("ID,Name,Description,Price,Category,Quantity,ExpDays\n");
             for (InventoryItem i : inventory) {
-
+                fw.append(itemToCsvString(i)).append("\n");
             }
 
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -31,8 +32,7 @@ public class CsvFileIO {
         sb.append(item.getCategory()).append(",");
         sb.append(item.getQuantity()).append(",");
 
-        if (item instanceof GroceryItem) {
-            GroceryItem groceryItem = (GroceryItem) item;
+        if (item instanceof GroceryItem groceryItem) {
             sb.append(groceryItem.getExpDays());
         }
         return sb.toString();
