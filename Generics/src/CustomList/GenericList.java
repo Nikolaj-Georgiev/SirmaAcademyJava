872780eb16d64edef055718606e133ec211f56ee
NGNikolay <georgiev.nikolaj@gmail.com>
2024-07-09 16:47:30 +0300

    added all the methods to the custom list and the test class

--- Generics/src/CustomList/GenericList.java
+++ Generics/src/CustomList/GenericList.java
@@ -53,4 +53,55 @@ public class GenericList<E extends Comparable<E>> {
         return removedElement;
     }
 
+    public int size() {
+        return size;
+    }
+
+    public boolean contains(E element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void swap(int i, int j) {
+        if (i == j) {
+            return;
+        }
+        if (i < 0 || i >= size || j < 0 || j >= size) {
+            throw new IndexOutOfBoundsException("One of the indexes is out of bound");
+        }
+        E temp = this.elements[i];
+        this.elements[i] = this.elements[j];
+        this.elements[j] = temp;
+    }
+
+    public E getMax() {
+        E max = this.elements[0];
+        for (int i = 1; i < size; i++) {
+            if (this.elements[i].compareTo(max) > 0) {
+                max = this.elements[i];
+            }
+        }
+        return max;
+    }
+
+    public E getMin() {
+        E min = this.elements[0];
+        for (int i = 1; i < size; i++) {
+            if (this.elements[i].compareTo(min) < 0) {
+                min = this.elements[i];
+            }
+        }
+        return min;
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(elements[i]);
+        }
+    }
+
 }
--- Generics/src/CustomList/TestCustomList.java
+++ Generics/src/CustomList/TestCustomList.java
@@ -1,7 +1,52 @@
 package CustomList;
 
+import java.util.Scanner;
+
 public class TestCustomList {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        GenericList<String> list = new GenericList<>();
+        String line = sc.nextLine();
+        System.out.println();
+        while (!line.equals("end")) {
+            String[] tokens = line.split(" ");
+
+            switch (tokens[0]) {
+                case "Add":
+                    list.add(tokens[1]);
+                    break;
+                case "Remove":
+                    String removed = list.remove(Integer.parseInt(tokens[1]));
+                    System.out.println(removed);
+                    break;
+                case "Contains":
+                    boolean found = list.contains(tokens[1]);
+                    System.out.println(found);
+                    break;
+                case "Swap":
+                    list.swap(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+                    break;
+                case "Print":
+                    list.print();
+                    break;
+                case "Max":
+                    System.out.println(list.getMax());
+                    break;
+                case "Min":
+                    System.out.println(list.getMin());
+                    break;
+                case "Greater":
+                    int greater = list.countGreaterThen(tokens[1]);
+                    System.out.println(greater);
+                    break;
+                default:
+                    throw new ClassNotFoundException("Unknown command! Try again :)");
+            }
+
+            line = sc.nextLine();
+        }
+
+        sc.close();
 
     }
 }
